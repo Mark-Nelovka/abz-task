074591c93342914f64b63bbe28c6dfc4148e0716
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-02-17 13:26:16 +0200

    added header for mobile version
    - added header
    - added general buttons
    - added styles
    - added new library (Material UI)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Header from "./Components/Header";
-import Hero from "./Components/Hero";
-import Cards from "./Components/Cards";
-import Form from "./Components/Form";
+// import Hero from "./Components/Hero";
+// import Cards from "./Components/Cards";
+// import Form from "./Components/Form";
 
 function App() {
   return (
     <>
       <Header />
-      <Hero />
+      {/* <Hero />
       <Cards />
-      <Form />
+      <Form /> */}
     </>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,21 @@
+import { Buttons } from "../../General/";
+import Logo from "../../assets/images/abz-logo.png";
+
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="container">
+        <div className="header-container">
+          <span>
+            <img src={Logo} alt="Logo" width={38} height={26} />
+          </span>
+          <span>TESTTASK</span>
+          <div className="buttons-container">
+            <Buttons text={"Users"} />
+            <Buttons text={"Sign up"} />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
